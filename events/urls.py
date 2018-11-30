1c2aa080fa77f0619087af8b3bc76e10aed54dd9
gaurav <gaurav.uttarkar@gmail.com>
2018-12-01 01:13:39 +0530

    Added comments

--- events/urls.py
+++ events/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     
     path('',views.index),
-    path('<str:eventId>',views.eventDetail)
+    path('marker',views.mapMarker, name="marker"),
+    path('comment',views.comments),
+    path('<str:eventId>',views.eventDetail),
+    
+
     # <a style="color:white" href='/stars/{{star.starName}}'>
 
 ]
--- events/views.py
+++ events/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from predicthq import Client
 from django.http import HttpResponse
 import requests
+from events.models import SafeLocation, DangerLocation, HelpLocation, Event\
+							,UserComments
 
 # Create your views here.
 
@@ -55,8 +57,104 @@ def eventDetail(request,eventId):
 	results = r.json()['results']
 	print(results)
 	results = results[0]
+	longi = results['location'][0]
+	lat = results['location'][1]
+	api = "147b79daa29884e1dab8fac91b7526d6"
+	print(type(lat))
+	url = "http://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(longi)+"&appid="+api;
+	response = requests.get(url)
+	print(type(response))
+	print(response.text)
+	res = response.json()
+	print(lat,longi)
+	#print(data)
+	temp = res['main']['temp']
+	pressure = res['main']['pressure']
+	humidity = res['main']['humidity']
+	temp_min = res['main']['temp_min']
+	temp_max = res['main']['temp_max']
+	windspeed = res['wind']['speed']
+	try:
+		winddeg = res['wind']['deg']
+	except:
+		winddeg = None	
+	mainweather	= res['weather'][0]['main']
+	description = res['weather'][0]['description']
 
-	return render(request,'eventDetail.html',{'event':results})
+	print(temp)
+	print(mainweather)
+	print(description)
+	safeLocation = []
+	dangerLocation = []
+	helpLocation = []
 
+	safeObj = SafeLocation.objects.all().filter(eventId=eventId)
+	dangerObj = DangerLocation.objects.all().filter(eventId=eventId)
+	helpObj = HelpLocation.objects.all().filter(eventId=eventId)
+
+	try:
+		obj = Event.objects.get(eventId=eventId)
+	except:
+		obj = Event.objects.create(eventId=eventId)
+		obj.save()
+
+	comments = UserComments.objects.all().filter(eventId=eventId)
+	print(type(comments))
+	
+	print(results['id'])
+
+	return render(request,'eventDetail.html',{'event':results,
+											  'weather':[mainweather,description],
+											  'temp':temp,
+											  'pressure':pressure,
+											  'humidity':humidity,
+											  'windspeed':windspeed,
+											  'winddeg': winddeg,
+											  'safeLocation':safeLocation,
+											  'dangerLocation': dangerLocation,
+											  'helpLocation': helpLocation,
+											  'comments': comments,
+												})
+
+def mapMarker(request):
+	lat = request.POST.get('lat')
+	lng = request.POST.get('lng')
+	eventId = request.POST.get('eventId')
+	color = request.POST.get('colour')
+	print(request.POST)
+	print(request.user)
+	if request.user:
+		if color == '0':
+			obj = Event.objects.get(eventId=eventId)
+			obj = HelpLocation.objects.create(eventId=obj,
+										latitude=lat,
+										longitude=lng)
+			obj.save()
+		if color == '1':
+			print('Red')
+			obj = Event.objects.get(eventId=eventId)
+			obj = DangerLocation.objects.create(eventId=obj,
+										latitude=lat,
+										longitude=lng)
+			obj.save()
+		if color == '2':
+			obj = Event.objects.get(eventId=eventId)
+			obj = SafeLocation.objects.create(eventId=obj,
+										latitude=lat,
+										longitude=lng)
+			obj.save()	
+				
+	#SafeLocation.objects.create()
+	return HttpResponse("Done")
+
+def comments(requests):
+	print(requests.POST)
+	eventId = requests.POST.get('eventId')
+	comment = requests.POST.get('comment')
+	eventObj = Event.objects.get(eventId=eventId)
+
+	commObj = UserComments.objects.create(userName=requests.user,eventId=eventObj,userComment=comment)
+	commObj.save()
+	return redirect("/events/"+eventId)
 
 
